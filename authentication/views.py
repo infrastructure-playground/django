3674895a8743b7fd286c3b0485facca376a6c4aa
dallas_city <dallas@unnotechmanila.com>
2021-08-19 22:16:40 +0800

    Initial Implementation of captcha

--- authentication/views.py
+++ authentication/views.py
@@ -12,6 +12,7 @@ from .serializers import AccountSerializer
 
 from django.contrib.auth.models import User
 
+from utils.functions import check_google_recaptcha
 from utils import constants
 
 
@@ -34,10 +35,18 @@ class AccountLogin(generics.CreateAPIView):
 
     def create(self, request, *args, **kwargs):
         """
-        :param request: {'username': 'armadadean', 'password': 'pass1234'}
+        :param request: {'username': 'armadadean',
+                         'password': 'pass1234',
+                         'token': 'xxxx'}
         :return:  {'token': 'abcdef123456'}
         """
-        account = authenticate(**request.data)
+        token = request.data['token']
+        print(f'token: {token}')
+        if not check_google_recaptcha(token):
+            raise serializers.ValidationError({'error': constants.ERROR_CAPTCHA})
+        username = request.data['username']
+        password = request.data['password']
+        account = authenticate(username=username, password=password)
         if not account:
             raise serializers.ValidationError({'error': constants.ERROR_AUTH})
         pre_payload = api_settings.JWT_PAYLOAD_HANDLER(account)
